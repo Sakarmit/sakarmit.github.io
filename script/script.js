b475411460c1902c3b31beb93516d58c61460612
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2024-07-24 18:08:46 -0400

    Automated Glossary Creation

---
+++ script/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//Glossary Generator
+Array.from(document.getElementsByClassName('glossary'))
+    .forEach((elem) => {
+        for (let item of document.body.getElementsByTagName('h5')) {
+            let name = item.id;
+            let text = item.textContent;
+            if (name) {
+                let link = document.createElement('a');
+                link.textContent = text;
+                link.href = `#${name}`
+                elem.append(link);
+            }
+        }
+    }
+);
