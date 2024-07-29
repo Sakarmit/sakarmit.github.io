db6dcdb7bc72274b73fe7c502a434fdac7f92cb1
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2024-07-29 18:57:38 -0400

    Heading tag decrement

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@
 //Glossary Generator
 Array.from(document.getElementsByClassName('glossary'))
     .forEach((elem) => {
-        for (let item of document.body.getElementsByTagName('h5')) {
+        for (let item of document.body.getElementsByTagName('h4')) {
             let name = item.id;
             let text = item.textContent;
             if (name) {
