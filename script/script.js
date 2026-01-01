677c6e4ae59304b90c655dc651f2db8bfdcf5709
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2025-12-31 22:18:03 -0500

    CSS/Theme Upgrades

--- script/script.js
+++ script/script.js
@@ -1,5 +1,43 @@
 'use strict';
 
+//Theme Selector
+const theme = localStorage.getItem('theme');
+if (theme === null || theme === 'dark') {
+    document.getElementsByTagName('html')[0].dataset.theme = 'dark';
+} else {
+    document.getElementsByTagName('html')[0].dataset.theme = 'light';
+}
+//Theme Selector Icon Positioning
+const themeIcon = document.getElementById('theme-icon');
+const themeSelected = document.getElementById('theme-selected');
+if (themeIcon && themeSelected) {
+    const updateThemeIconPosition = () => {
+        const rect = themeIcon.getBoundingClientRect();
+        themeSelected.style.left = `${rect.left + window.scrollX}px`;
+
+        let currentTheme = document.getElementsByTagName('html')[0].dataset.theme;
+        if (currentTheme === null || currentTheme === 'dark') {
+            themeSelected.style.top = `${rect.top + rect.height/2}px`;
+        } else {
+            themeSelected.style.top = `${rect.top}px`;
+        }
+    };
+
+    updateThemeIconPosition();
+    window.addEventListener('resize', updateThemeIconPosition);
+    document.getElementById('theme-selector').addEventListener('click', () => {
+        const currentTheme = document.getElementsByTagName('html')[0].dataset.theme;
+        if (currentTheme === null || currentTheme === 'dark') {
+            document.getElementsByTagName('html')[0].dataset.theme = 'light';
+            localStorage.setItem('theme', 'light');
+        } else {
+            document.getElementsByTagName('html')[0].dataset.theme = 'dark';
+            localStorage.setItem('theme', 'dark');
+        }
+        updateThemeIconPosition();
+    });
+}
+
 //Glossary Generator
 Array.from(document.getElementsByClassName('glossary'))
     .forEach((elem) => {
